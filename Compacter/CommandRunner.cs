339a84654d9943eab0dbb5bbb4bf36e97cc10214
Ytrog <hisbrucker@gmail.com>
2022-03-29 17:47:26 +0200

    Running the tasks

--- Compacter/CommandRunner.cs
+++ Compacter/CommandRunner.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Compacter
 {
@@ -11,7 +12,33 @@ namespace Compacter
 
         public static async Task Run(IEnumerable<CompactOptions> commands)
         {
-            throw new NotImplementedException();
+            List<Task> commandTasks = new List<Task>();
+            foreach (CompactOptions command in commands)
+            {
+                commandTasks.Add(RunCommand(command));
+            }
+
+            Task all = Task.WhenAll(commandTasks);
+            try
+            {
+                await all;
+            }
+            catch (AggregateException e)
+            {
+                // TODO report errors
+                throw;
+            }
+        }
+
+        private static async Task RunCommand(CompactOptions command)
+        {
+            ProcessStartInfo processStartInfo = new ProcessStartInfo
+            {
+                FileName = "compact.exe",
+                Arguments = command.Arguments
+            };
+
+            await Task.Run(() => Process.Start(processStartInfo));
         }
     }
 }
